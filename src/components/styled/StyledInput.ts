e05cf243d72deedc337c97b0b6decd8e4d00320a
Spencer <spencer@frozor.io>
2019-04-12 19:08:15 -0400

    Add readonly styles to styled input (w/ transition)

--- src/components/styled/StyledInput.ts
+++ src/components/styled/StyledInput.ts
@@ -5,6 +5,12 @@ const StyledInput = styled.input`
   border: none;
   border-radius: 0.25rem;
   background: #DDD;
+  transition: background-color 0.2s ease;
+  
+  :read-only {
+    background: #EEE;
+    cursor: not-allowed;
+  }
 `;
 
 const maxWidthStyle = { width: 'calc(100% - 1rem)' };
